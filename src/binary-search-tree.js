0a9d038bcd4624c4083bed859df2d353af8bb90d
Darya-Kazharnovich <darya.kazharnovich@gmail.com>
2022-05-02 19:28:39 +0300

    add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,24 +8,64 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.origin = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.origin;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.origin = addData(this.origin, data); // заполним значением из функции
+    function addData(node, data) { 
+      if (!node) { // если нет узла
+        return new Node(data); // создаем
+      }
+      if (node.data === data) { // если такое значение уже есть
+        return node; // не изменяем
+      }
+      if (data < node.data) { // если значение ниже, чем такущий узел
+        node.left = addData(node.left, data); // добавляем влево
+      } else {
+        node.right = addData(node.right, data); // если выше, чем текущий, то вправо
+      }
+      return node; 
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return hasData(this.origin, data) // возвратим true или false
+    function hasData(node, data) { 
+      if (!node) { // если нету, то false
+        return false;
+      }
+      if (node.data === data) { // если есть, то true
+        return true;
+      }
+      if (data < node.data) { 
+        return hasData(node.left, data); // если data < текущей, ищем слева
+    } else {
+        return hasData(node.right, data); // если data > текущей, ищем справа
+    }      
   }
+}
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findData(this.origin, data); // как та, что выше, только возвращаем null или значение
+    function findData(node, data) {
+      if (!node) {
+        return null;
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (data < node.data) {
+        return findData(node.left, data);
+    } else {
+        return findData(node.right, data);
+    }  
+    }
   }
 
   remove(/* data */) {
@@ -34,13 +74,25 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let node = this.origin;
+    if (!node) { // если нет узла, то null
+      return null;
+    }
+    while (node.left) { // минимальные значения находится максимально слева
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let node = this.origin;
+    if (!node) {
+      return null;
+    }
+    while (node.right) { // максимальные значения находится максимально справа
+      node = node.right;
+    }
+    return node.data;
   }
 }
 
